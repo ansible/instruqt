1f6f37a40887f01077c1ecf0bd054b5168e1b06a
sean cavanaugh <sean@cavanaugh.pro>
2024-10-04 16:45:27 -0400

    Update tablelink.js

--- assets/tablelink.js
+++ assets/tablelink.js
@@ -1,7 +1,19 @@
+// const tableRows = document.querySelectorAll(".table-clickable tbody tr");
+
+// for (const tableRow of tableRows) {
+//   tableRow.addEventListener("click", function () {
+//     window.location.href = this.dataset.href;
+//   });
+// }
+
 const tableRows = document.querySelectorAll(".table-clickable tbody tr");
 
 for (const tableRow of tableRows) {
-  tableRow.addEventListener("click", function () {
-    window.location.href = this.dataset.href;
+  tableRow.addEventListener("click", function (event) {
+    // Prevent the link's default behavior if clicked
+    event.preventDefault();
+
+    // Ensure the row's data-href link always wins
+    window.open(this.dataset.href, "_blank");
   });
 }
